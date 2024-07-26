9cf09732d1ece73ac7c932eaf13c06b07f67005f
power000403 <power000403@naver.com>
2024-07-26 17:07:31 +0900

    Periodic_Updates_24.07.26

--- Assets/Scripts/Controller/Item/AbsorbItemController.cs
+++ Assets/Scripts/Controller/Item/AbsorbItemController.cs
@@ -1,5 +1,13 @@
 
 public class AbsorbItemController : ItemController
 {
-    
+    #region Framework
+
+    protected override void Update()
+    {
+        base.Update();
+
+    }
+
+    #endregion
 }
