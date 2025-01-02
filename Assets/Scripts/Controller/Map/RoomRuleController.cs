03c55bdf778672800bfd252f4e61a1c2f74bf8ef
VeryMalangCow <127908142+VeryMalangCow@users.noreply.github.com>
2025-01-02 23:29:05 +0900

    Periodic_Updates_25.01.02_2
    1. 네비게이션 시스템을 조금 더 간편하게 제작
     => 인스펙터창에 일일이 넣어줘야 했던 불편함 개선
     => 순간적인 로딩이 생길 수 있지만, 크지 않음
     => 수정하는 것이 개발 측에서 큰 이점이 있어서 수정 적용

--- Assets/Scripts/Controller/Map/RoomRuleController.cs
+++ Assets/Scripts/Controller/Map/RoomRuleController.cs
@@ -59,7 +59,11 @@ public class RoomRuleController : MonoBehaviour
                 if (chile.gameObject.TryGetComponent(out WayPoint wp))
                 { InRoom_AllWayPoint.Add(wp); }
             }
+
+            // Waypoint Set
+            
         }
+
     }
 
     #endregion
@@ -68,6 +72,9 @@ public class RoomRuleController : MonoBehaviour
 
     public void Set_Completed()
     {
+        // Waypoint
+        SetOutData_WayPoint();
+
         // Extra Building
         if (InRoom_BuildThing != null && !InRoom_BuildThing.gameObject.activeSelf)
         {
@@ -82,6 +89,8 @@ public class RoomRuleController : MonoBehaviour
 
     public void Set_KillAll()
     {
+        SetInData_WayPoint();
+
         for (int i = 0; i < InRoom_AllEnemy.Count; i++)
         {
             if (InRoom_AllEnemy[i].EnemySpawnTF != null &&
@@ -94,6 +103,50 @@ public class RoomRuleController : MonoBehaviour
                 enemy.transform.position = InRoom_AllEnemy[i].EnemySpawnTF.transform.position;
                 enemy.gameObject.SetActive(true);
             }
+        }
+    }
+
+
+    private void SetInData_WayPoint()
+    {
+        for (int i = 0; i < InRoom_AllWayPoint.Count; i++)
+        {
+            InRoom_AllWayPoint[i].AdjacentWPList.Clear();
+            InRoom_AllWayPoint[i].AdjacentWPList = new List<WayPoint>();
+
+            for (int j = 0; j < InRoom_AllWayPoint.Count; j++)
+            {
+                if (InRoom_AllWayPoint[i] != InRoom_AllWayPoint[j])
+                {
+                    if (!IsExistWall(InRoom_AllWayPoint[i].transform, InRoom_AllWayPoint[j].transform))
+                    {
+                        InRoom_AllWayPoint[i].AdjacentWPList.Add(InRoom_AllWayPoint[j]);
+                    }
+                }
+            }
+        }
+    }
+    private void SetOutData_WayPoint()
+    {
+        for (int i = 0; i < InRoom_AllWayPoint.Count; i++)
+        {
+            InRoom_AllWayPoint[i].AdjacentWPList.Clear();
+            InRoom_AllWayPoint[i].AdjacentWPList = null;
+        }
+    }
+
+    private bool IsExistWall(Transform _StartTF, Transform _EndTF)
+    {
+        //Vector2 dirVec = _EndTF.position - _StartTF.position;
+        RaycastHit2D hit = Physics2D.Linecast(_StartTF.position, _EndTF.position, LayerMask.GetMask("Wall"));
+
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
