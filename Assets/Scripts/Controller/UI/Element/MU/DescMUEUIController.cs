b77501c923321a6e46f205f2565856b4087b0ee5
VeryMalangCow <a25495765@gmail.com>
2025-03-25 08:25:05 +0900

    Periodic_Updates_25.03.25_Refactoring(56)
    클래스 정리 (42) - 가독성의 위한 모듈화
    MUUIDescUI 리펙토링 완료

--- Assets/Scripts/Controller/UI/Element/MU/DescMUEUIController.cs
+++ Assets/Scripts/Controller/UI/Element/MU/DescMUEUIController.cs
@@ -25,10 +25,17 @@ public class DescMUEUIController : ElementUIController
     [SerializeField] public TMP_Text CurrentActualRankTxt;
     [SerializeField] private string ExtraString_Rank;
 
+    [Space(5)]
     [Header("-- MainChip")]
     [SerializeField] private GameObject MainChipGO;
+
+    [Space(3)]
+    [Header("* Locker")]
     [SerializeField] private Image RankLv3_LockerImg;
     [SerializeField] private Image RankLv5_LockerImg;
+
+    [Space(3)]
+    [Header("* Rank")]
     [SerializeField] private Image RankLv1_MainChipImg;
     [SerializeField] private List<Image> RankLv1_MainChipAmountImgs;
     [SerializeField] private Image RankLv3_MainChipImg;
@@ -63,7 +70,109 @@ public class DescMUEUIController : ElementUIController
 
         CurrentBoostLvMIAT.Set_Amount(0);
 
-        DevTool.Set_Color(PlayerManager.Instance.PlayerController.Get_CorrectColor(eDamageType.Energy, false), CurrentBoostLvMIAT.AmountImgs);
+        DevTool.Set_Color(clr, CurrentBoostLvMIAT.AmountImgs);
+    }
+
+    #endregion
+
+    #region Desc Element
+
+    private void SetOn_Item(int _ID, string _Name, string _Desc)
+    {
+        ItemIconImg.color = new Color(1, 1, 1, 1);
+        ItemIconImg.sprite = ModuleItemManager.Instance.Get_CorrectItemIcon(_ID);
+        ItemNameTxt.text = _Name;
+        ITemIntroTxt.text = _Desc;
+    }
+
+    private void SetOn_Rank(int _Rank)
+    {
+        CurrentRankImg.color = new Color(1, 1, 1, 1);
+        CurrentRankImg.sprite = ModuleItemManager.Instance.Get_CorrectDescRankIcon(_Rank);
+        CurrentRankTxt.text = ExtraString_Rank;
+        CurrentActualRankTxt.text = _Rank.ToString();
+    }
+
+    private void SetOn_MainChip(int _Rank, int _R1_ID, int _R3_ID, int _R5_ID)
+    {
+        MainChipGO.gameObject.SetActive(true);
+
+        RankLv1_MainChipImg.sprite = ModuleItemManager.Instance.Get_CorrectMainChip(_R1_ID).ThisIcon;
+        RankLv3_MainChipImg.sprite = ModuleItemManager.Instance.Get_CorrectMainChip(_R3_ID).ThisIcon;
+        RankLv5_MainChipImg.sprite = ModuleItemManager.Instance.Get_CorrectMainChip(_R5_ID).ThisIcon;
+
+        SetOff_AllLocker();
+        SetOff_AllMainChipImgs();
+
+        SetOn_Locker(_Rank);
+        SetOn_MainChipImgs(_Rank);
+    }
+
+    private void SetOn_MainChipImgs(int _Rank)
+    {
+        if (_Rank >= 5)
+        {
+            RankLv1_MainChipAmountImgs[0].gameObject.SetActive(true);
+            RankLv1_MainChipAmountImgs[1].gameObject.SetActive(true);
+            RankLv1_MainChipAmountImgs[2].gameObject.SetActive(true);
+
+            RankLv3_MainChipAmountImgs[0].gameObject.SetActive(true);
+            RankLv3_MainChipAmountImgs[1].gameObject.SetActive(true);
+
+            RankLv5_MainChipAmountImgs[0].gameObject.SetActive(true);
+        }
+        else if (_Rank >= 3)
+        {
+            RankLv1_MainChipAmountImgs[0].gameObject.SetActive(true);
+            RankLv1_MainChipAmountImgs[1].gameObject.SetActive(true);
+
+            RankLv3_MainChipAmountImgs[0].gameObject.SetActive(true);
+        }
+        else
+        {
+            RankLv1_MainChipAmountImgs[0].gameObject.SetActive(true);
+        }
+    }
+
+    private void SetOff_AllMainChipImgs()
+    {
+        for (int i = 0; i < RankLv1_MainChipAmountImgs.Count; i++)
+            RankLv1_MainChipAmountImgs[i].gameObject.SetActive(false);
+
+        for (int i = 0; i < RankLv3_MainChipAmountImgs.Count; i++)
+            RankLv3_MainChipAmountImgs[i].gameObject.SetActive(false);
+
+        for (int i = 0; i < RankLv5_MainChipAmountImgs.Count; i++)
+            RankLv5_MainChipAmountImgs[i].gameObject.SetActive(false);
+    }
+
+    private void SetOn_Locker(int _Rank)
+    {
+        if (_Rank >= 5)
+        {
+            RankLv3_LockerImg.gameObject.SetActive(false);
+            RankLv5_LockerImg.gameObject.SetActive(false);
+        }
+        else if (_Rank >= 3)
+        {
+            RankLv3_LockerImg.gameObject.SetActive(false);
+        }
+    }
+
+    private void SetOff_AllLocker()
+    {
+        RankLv3_LockerImg.gameObject.SetActive(true);
+        RankLv5_LockerImg.gameObject.SetActive(true);
+    }
+
+    private void SetOn_BoostLv(int _BoostLv)
+    {
+        Color clr = BoostLvImg.color;
+        clr.a = (float)_BoostLv / (float)PlayerController.MaxBoostLv;
+        BoostLvImg.color = clr;
+        CurrentBoostLvMIAT.Set_Amount(_BoostLv);
+        CurrentBoostLvTxt.text = ExtraString_BoostLv;
+        CurrentActualBoostLvTxt.text = _BoostLv.ToString();
     }
 
     #endregion
@@ -78,74 +187,17 @@ public class DescMUEUIController : ElementUIController
         { child.gameObject.SetActive(true); }
 
         // Item
-        ItemIconImg.color = new Color(1, 1, 1, 1);
-        ItemIconImg.sprite = ModuleItemManager.Instance.Get_CorrectItemIcon(_MS.ThisItemData.ID);
-        ItemNameTxt.text = _MS.ThisItemData.Name;
-        ITemIntroTxt.text = _MS.ThisItemData.Description;
+        SetOn_Item(_MS.ThisItemData.ID, _MS.ThisItemData.Name, _MS.ThisItemData.Description);
 
         // Rank
-        CurrentRankImg.color = new Color(1, 1, 1, 1);
-        CurrentRankImg.sprite = ModuleItemManager.Instance.Get_CorrectDescRankIcon(_MS.ThisItemData.Rank);
-        CurrentRankTxt.text = ExtraString_Rank;
-        CurrentActualRankTxt.text = _MS.ThisItemData.Rank.ToString();
+        SetOn_Rank(_MS.ThisItemData.Rank);
 
         // MainChip
-        MainChipGO.gameObject.SetActive(true);
-        RankLv1_MainChipImg.sprite =
-            ModuleItemManager.Instance.Get_CorrectMainChip(_MS.ThisItemData.R1_MainChipID).ThisIcon;
-        RankLv3_MainChipImg.sprite =
-            ModuleItemManager.Instance.Get_CorrectMainChip(_MS.ThisItemData.R3_MainChipID).ThisIcon;
-        RankLv5_MainChipImg.sprite =
-            ModuleItemManager.Instance.Get_CorrectMainChip(_MS.ThisItemData.R5_MainChipID).ThisIcon;
-
-        RankLv3_LockerImg.gameObject.SetActive(true);
-        RankLv5_LockerImg.gameObject.SetActive(true);
-
-
-        RankLv1_MainChipAmountImgs[0].gameObject.SetActive(false);
-        RankLv1_MainChipAmountImgs[1].gameObject.SetActive(false);
-        RankLv1_MainChipAmountImgs[2].gameObject.SetActive(false);
-
-        RankLv3_MainChipAmountImgs[0].gameObject.SetActive(false);
-        RankLv3_MainChipAmountImgs[1].gameObject.SetActive(false);
-
-        RankLv5_MainChipAmountImgs[0].gameObject.SetActive(false);
-
-        if (_MS.ThisItemData.Rank >= 5)
-        {
-            RankLv3_LockerImg.gameObject.SetActive(false);
-            RankLv5_LockerImg.gameObject.SetActive(false);
-
-            RankLv1_MainChipAmountImgs[0].gameObject.SetActive(true);
-            RankLv1_MainChipAmountImgs[1].gameObject.SetActive(true);
-            RankLv1_MainChipAmountImgs[2].gameObject.SetActive(true);
-
-            RankLv3_MainChipAmountImgs[0].gameObject.SetActive(true);
-            RankLv3_MainChipAmountImgs[1].gameObject.SetActive(true);
-
-            RankLv5_MainChipAmountImgs[0].gameObject.SetActive(true);
-        }
-        else if (_MS.ThisItemData.Rank >= 3)
-        {
-            RankLv3_LockerImg.gameObject.SetActive(false);
-
-            RankLv1_MainChipAmountImgs[0].gameObject.SetActive(true);
-            RankLv1_MainChipAmountImgs[1].gameObject.SetActive(true);
-
-            RankLv3_MainChipAmountImgs[0].gameObject.SetActive(true);
-        }
-        else
-        {
-            RankLv1_MainChipAmountImgs[0].gameObject.SetActive(true);
-        }
+        SetOn_MainChip(_MS.ThisItemData.Rank,
+            _MS.ThisItemData.R1_MainChipID, _MS.ThisItemData.R3_MainChipID, _MS.ThisItemData.R5_MainChipID);
 
         // Boost Lv
-        Color clr = BoostLvImg.color;
-        clr.a = (float)_MS.ThisItemData.BoostLv / (float)PlayerController.MaxBoostLv;
-        BoostLvImg.color = clr;
-        CurrentBoostLvMIAT.Set_Amount(_MS.ThisItemData.BoostLv);
-        CurrentBoostLvTxt.text = ExtraString_BoostLv;
-        CurrentActualBoostLvTxt.text = _MS.ThisItemData.BoostLv.ToString();
+        SetOn_BoostLv(_MS.ThisItemData.BoostLv);
     }
 
     public void SetOff_Desc()
